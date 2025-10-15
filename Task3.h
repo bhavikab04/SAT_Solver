ee243e2b0c8885b6fb6b0cd326c6242140c541fa
bhavikab04 <bhavikababuraj04@gmail.com>
2025-10-16 01:30:21 +0530

    task3 header file updated

--- Task3.h
+++ Task3.h
@@ -1,12 +1,6 @@
 #ifndef TASK3_H
 #define TASK3_H
 
-typedef struct TreeNode
-{
-    char data;
-    struct TreeNode *left;  // Points to Binary ops
-    struct TreeNode *right; // Points to all all ops
-} TreeNode;
 
 //Expression length calculator function:
 int getExpLength(TreeNode* root);
