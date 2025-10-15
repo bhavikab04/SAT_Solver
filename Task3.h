feafcb434db07ffadce4bbf6d0aa7b4703f8b514
labphoenixx <143266088+labphoenixx@users.noreply.github.com>
2025-10-16 01:21:49 +0530

    Rename Node to TreeNode in Task3.h

--- Task3.h
+++ Task3.h
@@ -1,20 +1,20 @@
 #ifndef TASK3_H
 #define TASK3_H
 
-typedef struct Node
+typedef struct TreeNode
 {
     char data;
-    struct Node *left;  // Points to Binary ops
-    struct Node *right; // Points to all all ops
-} Node;
+    struct TreeNode *left;  // Points to Binary ops
+    struct TreeNode *right; // Points to all all ops
+} TreeNode;
 
 //Expression length calculator function:
-int getExpLength(Node* root);
+int getExpLength(TreeNode* root);
 
 //In Order traversal function:
-void inOrderTraversal(Node* root, char* bufferStr, int* pos);
+void inOrderTraversal(TreeNode* root, char* bufferStr, int* pos);
 
 //Free all memory allocated to parse tree (using post-order traversal):
-void freeTree(Node* root);
+void freeTree(TreeNode* root);
 
 #endif
