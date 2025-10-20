d8dd2e72c1a0ea432262be1639ee9d6d8742b9d6
sripujithak <spkonjeti@gmail.com>
2025-10-21 00:06:23 +0530

    added documentation

--- Task3.h
+++ Task3.h
@@ -1,14 +1,54 @@
 #ifndef TASK3_H
 #define TASK3_H
 
+/**
+ * @file Task3.h
+ * @brief Header file for Task 3: Parse Tree to Infix Conversion.
+ *
+ * Declares functions to convert the parse tree back into an
+ * infix expression string.
+ *
+ * @note This header requires the `TreeNode` structure definition,
+ * which is assumed to be defined (e.g., in "Task2.h").
+ */
 
-//Expression length calculator function:
+
+/**
+ * @brief Calculates the required string length for the infix expression.
+ *
+ * Traverses the tree to determine the exact buffer size needed to store
+ * the infix representation, including all operands, operators,
+ * and necessary parentheses.
+ *
+ * @param root The root of the parse tree.
+ * @return The total length (number of characters) of the infix string.
+ */
 int getExpLength(TreeNode* root);
 
-//In Order traversal function:
+/**
+ * @brief Reconstructs the infix expression string using in-order traversal.
+ *
+ * Recursively visits the tree nodes in-order (left, root, right) and
+ * writes the corresponding characters (operands, operators, parentheses)
+ * into the provided buffer.
+ *
+ * @param root The current node being visited in the tree.
+ * @param bufferStr A pointer to the character buffer to write into.
+ * @param pos A pointer to an integer tracking the current write
+ * position (index) in the `bufferStr`.
+ */
 void inOrderTraversal(TreeNode* root, char* bufferStr, int* pos);
 
-//Free all memory allocated to parse tree (using post-order traversal):
+/**
+ * @brief Frees all memory allocated to the parse tree.
+ *
+ * Recursively frees all nodes and their associated string data
+ * using a post-order traversal.
+ *
+ * @note This function is typically defined in Task 2 (task2.c)
+ * and re-declared here for use by Task 3.
+ * @param root The root of the tree (or subtree) to be freed.
+ */
 void freeTree(TreeNode* root);
 
-#endif
+#endif // TASK3_H
